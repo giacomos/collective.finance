ece7e2b3505051228b207e3ce011f601b188f903
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2013-10-26 15:28:22 +0200

    Try to fix travis build

--- collective/finance/__init__.py
+++ collective/finance/__init__.py
@@ -1,7 +1,3 @@
 from zope.i18n import MessageFactory
 
 messageFactory = MessageFactory("collective.finance")
-
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
