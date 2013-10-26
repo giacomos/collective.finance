92c0f474681e005325eb97dbe9a1753bffeaaaa0
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2013-10-26 16:23:57 +0200

    Improved classifiers list

--- setup.py
+++ setup.py
@@ -2,18 +2,23 @@ from setuptools import setup, find_packages
 import os
 
 version = '0.1'
+long_description = open("README.rst").read() + "\n" + \
+    open(os.path.join("docs", "HISTORY.txt")).read(),
 
 setup(name='collective.finance',
       version=version,
       description="",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Development Status :: 2 - Pre-Alpha",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+      ],
       keywords='Plone Python Finance Manager',
       author='Giacomo Spettoli',
       author_email='giacomo.spettoli@gmail.com',
