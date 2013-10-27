9de3da6127ee4bea0351214e059dbfcf99916bb0
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2013-10-27 16:49:34 +0100

    Almost finished qif import

--- collective/finance/browser/importQIF.py
+++ collective/finance/browser/importQIF.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import StringIO
 import z3c.form.button
 import z3c.form
 from datetime import datetime
@@ -7,6 +6,7 @@ from Products.CMFCore.utils import getToolByName
 from collective.finance.interfaces import IImportQIFFormSchema
 from collective.finance import messageFactory as _
 from Products.statusmessages.interfaces import IStatusMessage
+
 
 def convertDate(qdate):
     """ convert from QIF time format to ISO date string
@@ -23,17 +23,20 @@ def convertDate(qdate):
     for i in range(len(qdate)):
         if qdate[i] == " ":
             qdate = qdate[:i] + "0" + qdate[i+1:]
-    if len(qdate) == 10: # new form with YYYY date
+    if len(qdate) == 10:  # new form with YYYY date
         return qdate[6:10] + "-" + qdate[0:2] + "-" + qdate[3:5]
-    if qdate[5] == "'": C="20"
-    else: C="19"
+    if qdate[5] == "'":
+        C = "20"
+    else:
+        C = "19"
     return C + qdate[6:8] + "-" + qdate[0:2] + "-" + qdate[3:5]
+
 
 class QifItem(object):
     def __init__(self):
         self.order = ['date', 'amount', 'cleared', 'num', 'payee', 'memo',
                       'address', 'category', 'categoryInSplit',
-                      'memoInSplit', 'amountOfSplit']
+                      'memoInSplit', 'amountOfSplit', 'toAccount']
         self.date = None
         self.amount = None
         self.cleared = None
@@ -45,6 +48,7 @@ class QifItem(object):
         self.categoryInSplit = None
         self.memoInSplit = None
         self.amountOfSplit = None
+        self.toAccount = None
 
     def show(self):
         pass
@@ -66,7 +70,7 @@ class QifItem(object):
         return tmpstring
 
 
-def parseQif(infile):
+def parseQif(ops):
     """
     Parse a qif file and return a list of entries.
     infile should be open file-like object (supporting readline() ).
@@ -74,49 +78,80 @@ def parseQif(infile):
 
     items = []
     curItem = QifItem()
-    line = infile.readline()
-    while line != '':
-        if line[0] == '\n':  # blank line
-            pass
-        elif line[0] == '^':  # end of item
-            # save the item
-            items.append(curItem)
-            curItem = QifItem()
-        elif line[0] == 'D':
-            curItem.date = line[1:-1]
-        elif line[0] == 'T':
-            curItem.amount = line[1:-1]
-        elif line[0] == 'C':
-            curItem.cleared = line[1:-1]
-        elif line[0] == 'P':
-            curItem.payee = line[1:-1]
-        elif line[0] == 'M':
-            curItem.memo = line[1:-1]
-        elif line[0] == 'A':
-            curItem.address = line[1:-1]
-        elif line[0] == 'L':
-            curItem.category = line[1:-1]
-        elif line[0] == 'S':
-            try:
-                curItem.categoryInSplit.append(";" + line[1:-1])
-            except AttributeError:
-                curItem.categoryInSplit = line[1:-1]
-        elif line[0] == 'E':
-            try:
-                curItem.memoInSplit.append(";" + line[1:-1])
-            except AttributeError:
-                curItem.memoInSplit = line[1:-1]
-        elif line[0] == '$':
-            try:
-                curItem.amountInSplit.append(";" + line[1:-1])
-            except AttributeError:
-                curItem.amountInSplit = line[1:-1]
-        else:
-            # don't recognise this line; ignore it
-            print ("Skipping unknown line:\n" + line)
-
-        line = infile.readline()
+    for op in ops:
+        op = op.startswith('\n') and op[1:] or op
+        op = op.endswith('\n') and op[:-1] or op
+        lines = op.split('\n')
+        curItem = QifItem()
+        for line in lines:
+            if not len(line) or line[0] == '\n' or line.startswith('!Type'):
+                continue
+            elif line[0] == 'D':
+                curItem.date = line[1:]
+            elif line[0] == 'T':
+                curItem.amount = line[1:]
+            elif line[0] == 'C':
+                curItem.cleared = line[1:]
+            elif line[0] == 'P':
+                curItem.payee = line[1:]
+            elif line[0] == 'M':
+                curItem.memo = line[1:]
+            elif line[0] == 'A':
+                curItem.address = line[1:]
+            elif line[0] == 'L':
+                cat = line[1:]
+                if cat.startswith('['):
+                    curItem.toAccount = cat[1:-1]
+                else:
+                    curItem.category = cat
+            elif line[0] == 'S':
+                try:
+                    curItem.categoryInSplit.append(";" + line[1:-1])
+                except AttributeError:
+                    curItem.categoryInSplit = line[1:-1]
+            elif line[0] == 'E':
+                try:
+                    curItem.memoInSplit.append(";" + line[1:-1])
+                except AttributeError:
+                    curItem.memoInSplit = line[1:-1]
+            elif line[0] == '$':
+                try:
+                    curItem.amountInSplit.append(";" + line[1:-1])
+                except AttributeError:
+                    curItem.amountInSplit = line[1:-1]
+            else:
+                # don't recognise this line; ignore it
+                print ("Skipping unknown line:\n" + str(line))
+        items.append(curItem)
     return items
+
+
+def getAccounts(chunks):
+    res = []
+    idx = 0
+    for chunk in chunks:
+        if '!Account' in chunk:
+            parts = chunk.split('\n')
+            account = {}
+            account['title'] = parts[2][1:]
+            account['type'] = parts[3][1:]
+            account['idx'] = idx
+            res.append(account)
+        idx += 1
+    return res
+
+
+def getTrByAccount(chunks, accounts):
+    num = len(accounts)
+    for i in range(num):
+        if i < num - 1:
+            slice_start = accounts[i]['idx'] + 1
+            slice_end = accounts[i + 1]['idx']
+        else:
+            slice_start = accounts[i]['idx'] + 1
+            slice_end = len(chunks)
+        accounts[i]['ops'] = chunks[slice_start:slice_end]
+    return accounts
 
 
 class ImportQIFView(z3c.form.form.Form):
@@ -132,26 +167,52 @@ class ImportQIFView(z3c.form.form.Form):
     def processQIF(self, data):
         """
         """
-        io = StringIO.StringIO(data)
-        items = parseQif(io)
+
         catalog = getToolByName(self.context, 'portal_catalog')
         path = '/'.join(self.context.getPhysicalPath())
         brains = catalog(path=path, portal_type="FinanceTransaction")
         num = len(brains)
-        for item in items:
-            next_id = 'transaction-%d' % (num + 1)
-            num += 1
-            obj_id = self.context.invokeFactory('FinanceTransaction', next_id)
+
+        chunks = data.split('^')
+        accounts = getAccounts(chunks)
+        accounts = getTrByAccount(chunks, accounts)
+        acc_uids = {}
+        for account in accounts:
+            title = account['title']
+            obj_id = self.context.invokeFactory('FinanceAccount', title)
             obj = self.context[obj_id]
-            iso_date = convertDate(item.date)
-            obj.date = datetime.strptime(iso_date, '%Y-%M-%d')
-            obj.amount = float(item.amount)
-            obj.address = item.address
-            obj.memo = item.memo
-            obj.income_expense = item.amount.startswith('-') and 'Expense' \
-                or 'Income'
-            import pdb; pdb.set_trace()
-        return len(items)
+            obj.account_type = account['type']
+            obj.title = title
+            obj.reindexObject()
+            acc_uids[title] = obj.UID()
+        for account in accounts:
+            items = parseQif(account['ops'])
+
+            for item in items:
+                if not item.date:
+                    continue
+                next_id = 'transaction-%d' % (num + 1)
+                num += 1
+                if item.toAccount:
+                    obj_id = self.context.invokeFactory('FinanceTransfer',
+                                                        next_id)
+                else:
+                    obj_id = self.context.invokeFactory('FinanceTransaction',
+                                                        next_id)
+                obj = self.context[obj_id]
+                iso_date = convertDate(item.date)
+                obj.date = datetime.strptime(iso_date, '%Y-%M-%d')
+                obj.amount = float(item.amount)
+                obj.address = item.address
+                obj.memo = item.memo
+                obj.account = acc_uids[account['title']]
+                if item.toAccount:
+                    obj.to_account = acc_uids[item.toAccount]
+                else:
+                    obj.income_expense = item.amount.startswith('-') and \
+                        'Expense' or 'Income'
+                obj.reindexObject()
+        return 'FATTO!!'
 
     @z3c.form.button.buttonAndHandler(_('Import'), name='import')
     def importQIF(self, action):
